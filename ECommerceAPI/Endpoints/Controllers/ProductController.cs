39280c49d4564f09b5f124576f4e3d152d242c5c
Joey Weixel <Joey.Weixel@neudesic.com>
2024-05-15 13:57:17 -0400

    Create get for all products

--- ECommerceAPI/Endpoints/Controllers/ProductController.cs
+++ ECommerceAPI/Endpoints/Controllers/ProductController.cs
@@ -1,6 +1,28 @@
-﻿namespace ECommerceAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.Controllers
 {
-    public class ProductController
+    public class ProductController : ControllerBase
     {
+        private readonly ProductService _service;
+
+        public ProductController(ProductService service) {
+            _service = service;
+        }
+
+        [HttpGet("./Products")]
+        public IActionResult GetAllProducts()
+        {
+            try
+            {
+                var products = _service.GetAllProducts();
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
     }
 }
