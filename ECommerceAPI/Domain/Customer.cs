9483532ece655ef3ce5a5dca2e9a93d55ca0b468
Izayah Gibson <izayahgibson@gmail.com>
2024-05-15 13:48:35 -0400

    Updated location of domains to be inside of the sln

---
+++ ECommerceAPI/Domain/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceAPI.Domain
+{
+    public class Customer
+    {
+        public Cart Cart { get; set; }
+        public ContactInfo ContactInfo { get; set; }
+        public PaymentInfo PaymentInfo { get; set; }
+        public History History { get; set; }
+    }
+}
+}
---
+++ ECommerceAPI/Domain/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceAPI.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public int numInStock { get; set; }
+        public decimal Price { get; set; }
+    }
+}
