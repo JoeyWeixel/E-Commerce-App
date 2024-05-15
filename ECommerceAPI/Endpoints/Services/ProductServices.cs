3a8994d026eeb376a1c625ca2ecf9207662c7aaa
Joey Weixel <Joey.Weixel@neudesic.com>
2024-05-15 15:36:22 -0400

    Add stub methods in product services, create response and request objects

---
+++ ECommerceAPI/Endpoints/Services/ProductServices.cs
@@ -0,0 +1,30 @@
+﻿using ECommerceAPI.Endpoints.ResponseObjects;
+
+namespace ECommerceAPI.Endpoints.Services
+{
+    public class ProductServices
+    {
+
+        public ProductServices() { }
+
+        public ProductResponse[] GetProducts()
+        {
+            //TODO: Reference database which is yet to be created
+            ProductResponse[] products = new ProductResponse[1];
+        }
+
+        public ProductResponse GetProduct(int id) {
+            //TODO query database
+            return new ProductResponse();
+        }
+
+        public void DeleteProduct(int id){
+            //TODO
+        }
+
+        public void AddProduct(ProductRequest product)
+        {
+
+        }
+    }
+}
