408c0b7131a669541fabfabeb83087c1f767d7ec
Izayah Gibson <izayahgibson@gmail.com>
2024-05-15 13:14:43 -0400

    Updated structure/folder structure

---
+++ Domain/Product.cs
@@ -0,0 +1,12 @@
+namespace Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public int numInStock { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
