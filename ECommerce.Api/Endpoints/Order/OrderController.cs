96f3952fb096ad016789defdf421e1f46c320711
Joey Weixel <Joey.Weixel@neudesic.com>
2024-05-16 12:22:52 -0400

    Add basic order service

---
+++ ECommerce.Api/Endpoints/Order/OrderController.cs
@@ -0,0 +1,55 @@
+﻿using ECommerce.Api.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Api.Endpoints.History
+{
+    public class OrderController : ControllerBase
+    {
+
+        OrderService _service;
+        public OrderController(OrderService service) { 
+            _service = service;
+        }
+
+        [HttpPost("/orders")]
+        public IActionResult AddOrder(OrderRequest order) {
+            try
+            {
+                var newOrder = _service.AddOrder(order);
+                return Created($"/history/{newOrder.id}",newOrder);
+            }
+            catch (System.Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("/orders/{id}")]
+        public IActionResult GetOrder(int id)
+        {
+            try
+            {
+                var order = _service.GetOrder(id);
+                return Ok(order);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("/orders")]
+        public IActionResult GetAllOrders() {
+            try
+            {
+                var orders = _service.GetAllOrders();
+                return Ok(orders);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ ECommerce.Api/Endpoints/Order/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ECommerce.Api.Endpoints.Order
+{
+    public class OrderService
+    {
+        public OrderService() { }
+
+        public IEnumerable<OrderResponse> GetAllOrders()
+        {
+            var orders = new List<OrderResponse>();
+            return orders;
+        }
+
+        public OrderResponse GetOrder(int id)
+        {
+            var order = new OrderResponse();
+            return order;
+        }
+
+        public Order AddOrder AddOrder(OrderRequest order)
+        {
+            var newOrder = new Order(order);
+            return newOrder;
+        }
+    }
+}
