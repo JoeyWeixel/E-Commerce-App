6eeb4eca7f196ae864ded2daa2be4cfc87bbc414
Joey Weixel <Joey.Weixel@neudesic.com>
2024-05-16 12:33:01 -0400

    Actually add order request/response and use system for errors

---
+++ ECommerce.Api/Endpoints/Order/RequestResponse/OrderRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.Api.Endpoints.Order.RequestResponse
+{
+    public class OrderRequest
+    {
+        public int Id { get; set; }
+        public ECommerceAPI.Domain.Cart Cart { get; set; }
+        public System.DateTime OrderDate { get; set; }
+
+    }
+}
---
+++ ECommerce.Api/Endpoints/Order/RequestResponse/OrderResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.Api.Endpoints.Order.RequestResponse
+{
+    public class OrderResponse
+    {
+        public int Id { get; set; }
+        public Cart Cart { get; set; }
+        public System.DateTime? DeliveryDate { get; set; }
+        public System.DateTime OrderDate { get; set; }
+    }
+}
