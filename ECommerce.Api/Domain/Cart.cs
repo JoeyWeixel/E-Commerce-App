4e9d6f347087ff2e80234839821a7d655393f5c0
Andrew Pla <andrew.pla@neudesic.com>
2024-05-16 10:57:34 -0400

    Code Review

---
+++ ECommerce.Api/Domain/Cart.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceAPI.Domain
+{
+    public class Cart
+    {
+        // dictionary of products and the quantity of each product in the cart.
+        public Dictionary<Product, int> Products { get; set; }
+        public int Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
