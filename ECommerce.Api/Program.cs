2b41047f84afa34fe4a746a0d65533535c1af548
Henry <Henry.Fritz@neudesic.com>
2024-05-16 13:30:06 -0400

    database fixes

--- ECommerce.Api/Program.cs
+++ ECommerce.Api/Program.cs
@@ -1,7 +1,4 @@
 using ECommerceAPI.Endpoints.Product;
-using ECommerceAPI.Endpoints.Customer;
-
-
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<ECommerceContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<ProductService>();
