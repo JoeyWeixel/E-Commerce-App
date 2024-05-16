b8b9fee6838478732240b9119c064014216aeae5
Gavin <gshi4three@gmail.com>
2024-05-16 12:37:59 -0400

    Updating domain object variables

--- ECommerce.Api/Domain/Customer.cs
+++ ECommerce.Api/Domain/Customer.cs
@@ -2,11 +2,11 @@
 {
     public class Customer
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public Cart Cart { get; set; }
         public ContactInfo ContactInfo { get; set; }
-        //public PaymentInfo PaymentInfo { get; set; }
-        //public History History { get; set; }
+        public PaymentInfo PaymentInfo { get; set; }
+        public History History { get; set; }
     }
 }
 
--- ECommerce.Api/Domain/Product.cs
+++ ECommerce.Api/Domain/Product.cs
@@ -5,8 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int numInStock { get; set; }
+        public int Stock { get; set; }
         public double Price { get; private set; }
-
     }
 }
