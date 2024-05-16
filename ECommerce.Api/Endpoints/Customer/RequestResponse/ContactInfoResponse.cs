621b8ee5afdb9fae847be06c21f3dc258519d6a8
Izayah Gibson <izayahgibson@gmail.com>
2024-05-16 13:24:57 -0400

    Fixed merge conflict

---
+++ ECommerce.Api/Endpoints/Customer/RequestResponse/ContactInfoResponse.cs
@@ -0,0 +1,9 @@
+namespace ECommerceAPI.Endpoints.Customer.RequestResponse;
+
+public class GetContactInfoResponse
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+}
