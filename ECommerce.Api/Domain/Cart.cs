2523c7d87d3a3bad9f37c99c8e637add9f5371c1
Gavin <gshi4three@gmail.com>
2024-05-20 12:49:20 -0400

    Adding cart methods and updating response file

--- ECommerce.Api/Domain/Cart.cs
+++ ECommerce.Api/Domain/Cart.cs
@@ -2,7 +2,7 @@
 {
     public class Cart
     {
-        //public Dictionary<Product, int> Products { get; set; }
+        public Dictionary<Product, int> Products { get; set; }
         public int Id { get; set; }
         public double TotalPrice { get; set; }
     }
--- ECommerce.Api/Endpoints/Customer/RequestResponse/Cart/CartResponse.cs
+++ ECommerce.Api/Endpoints/Customer/RequestResponse/Cart/CartResponse.cs
@@ -7,5 +7,11 @@ namespace ECommerceAPI.Endpoints.CustomerEndpoint.RequestResponse
         public Dictionary<ProductResponse, int> Products { get; set; }
         public int Id { get; set; }
         public decimal TotalPrice { get; set; }
+
+        // Constructor to initialize the Products dictionary
+        public CartResponse()
+        {
+            Products = new Dictionary<ProductResponse, int>();
+        }
     }
 }
