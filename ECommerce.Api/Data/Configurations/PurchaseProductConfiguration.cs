75fc1834e00fb104ce0c7d947de7d10c7014f57f
Joey Weixel <Joey.Weixel@neudesic.com>
2024-05-29 14:56:45 -0400

    purchaseproduct config

---
+++ ECommerce.Api/Data/Configurations/PurchaseProductConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using ECommerceAPI.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerceAPI.Data.Configurations
+{
+    public class PurchaseProductConfiguration : IEntityTypeConfiguration<PurchaseProduct>
+    {
+        public void Configure(EntityTypeBuilder<PurchaseProduct> builder)
+        {
+            builder.ToTable("Product");
+            builder.Property(x => x.Cart)
+                .IsRequired();
+            builder.Property(x => x.Product)
+                .IsRequired();
+            builder.HasKey(x => new { x.Cart, x.Product });
+        }
+    }
+}
