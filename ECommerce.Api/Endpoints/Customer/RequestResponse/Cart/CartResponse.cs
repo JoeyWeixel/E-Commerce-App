62229b046646e91edb6d861428e9a1fa0af4d79d
Izayah Gibson <izayahgibson@gmail.com>
2024-06-05 12:34:30 -0400

    Altered CartResponse product list type

--- ECommerce.Api/Endpoints/Customer/RequestResponse/Cart/CartResponse.cs
+++ ECommerce.Api/Endpoints/Customer/RequestResponse/Cart/CartResponse.cs
@@ -4,21 +4,21 @@ namespace ECommerceAPI.Endpoints.CustomerEndpoint.RequestResponse
 {
     public class CartResponse
     {
-        public List<PurchaseProduct> Products { get; set; }
+        public List<PurchaseProductResponse> Products { get; set; }
         public int Id { get; set; }
         public CartResponse(int customerId)
         {
-            Products = new List<PurchaseProduct>();
+            Products = new List<PurchaseProductResponse>();
             Id = customerId;
         }
         public CartResponse(Cart cart) { }
-        public void AddProduct(PurchaseProduct product)
-        {
-            Products.Add(product);
-        }
-        public void RemoveProduct(PurchaseProduct product)
-        {
-            Products.Remove(product);
-        }
+        /*public void AddProduct(PurchaseProduct product)
+       {
+           Products.Add(product);
+       }
+       public void RemoveProduct(PurchaseProduct product)
+       {
+           Products.Remove(product);
+       }*/
     }
 }
