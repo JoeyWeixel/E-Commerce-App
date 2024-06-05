36ee8f9558d05d04b561377fa9633c7d89ba8023
Henry <Henry.Fritz@neudesic.com>
2024-06-05 14:59:43 -0400

    package lock inside of ecommerce.web removed

--- ECommerceDatabase/Product.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Product]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] NVARCHAR(50) NOT NULL, 
-    [Description] NVARCHAR(MAX) NULL, 
-    [Price] DECIMAL(18, 2) NOT NULL, 
-    [NumInStock] INT NOT NULL,
-)
