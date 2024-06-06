811e1e2f222649223aeed8941c41071e0b9b676e
Joey Weixel <Joey.Weixel@neudesic.com>
2024-06-06 11:26:35 -0400

    Style carousel on home page

--- ECommerce.Web/src/Pages/HomePage.tsx
+++ ECommerce.Web/src/Pages/HomePage.tsx
@@ -9,6 +9,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/Components/ui/carousel"
+import { Label } from "@/Components/ui/label";
 
 interface ProductType {
   id: number;
@@ -38,18 +39,21 @@ const HomePage: React.FC<HomePageProps> = () => {
   }, []);
 
   return (
-    <div className="bg-[url('https://www.thevillagernewspaper.com/wordpress/wp-content/uploads/2018/04/RRMathCounts.jpg')] w-full h-full bg-cover">
-      <Carousel className="w-2/3 mx-auto mt-16 bg-primary">
-        <CarouselContent>
-          {products.map((product) => (
-            <CarouselItem className="basis-1/6 grow" key={product.id}>
-              <Product product={product} />
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+    <div className="bg-[url('https://www.thevillagernewspaper.com/wordpress/wp-content/uploads/2018/04/RRMathCounts.jpg')] w-full h-full bg-cover flex flex-col pt-16">
+      <Label className="text-4xl text-center text-card-foreground bg-card mx-auto p-4 rounded-md underline decoration-2 decoration-secondary underline-offset-8">Our Products</Label>
+      <div className="mx-auto max-w-max min-w-[500px] flex justify-center items-start my-4 bg-card py-4 rounded-xl border-2 border-secondary">
+        <Carousel className="w-full mx-16 flex justify-center">
+          <CarouselContent className="">
+            {products.map((product) => (
+              <CarouselItem className="basis-1/3 grow min-w-[200px]" key={product.id}>
+                <Product product={product} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="border-2"/>
+          <CarouselNext className="border-2"/>
+        </Carousel>
+      </div>
     </div>
   );
 };
