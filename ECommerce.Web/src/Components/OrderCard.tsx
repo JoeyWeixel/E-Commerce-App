0a341a319be56873d62121a82a7a8547fd5e70b9
Joey Weixel <Joey.Weixel@neudesic.com>
2024-06-10 12:16:39 -0400

    Order page fully styled

--- ECommerce.Web/src/Components/OrderCard.tsx
+++ ECommerce.Web/src/Components/OrderCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, CardHeader } from '@/Components/ui/card';
 import { OrderType } from '@/Pages/OrdersPage';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './ui/accordion';
 
 
 type OrderCardProps = {
@@ -7,16 +8,40 @@ type OrderCardProps = {
 }
 
 const OrderCard: React.FC<OrderCardProps> = ({ order }) => {
+    const date = new Date(order.orderDate);
+
+    const getOrderTotal = () => {
+        return order.products.reduce((accumulator, product) => accumulator + (product.price)*(product.quantity), 0);
+    }
+
     return (
         <Card>
             <CardHeader>
                 <h3 className="text-lg mx-auto">Order #{order.id}</h3>
             </CardHeader>
             <CardContent>
-                <p>Order Date: {order.orderDate}</p>
-                <p>Order Total: {
-                    order.products.reduce((accumulator, product) => accumulator + (product.price)*(product.quantity), 0)
-                }</p>
+                <Accordion type='single' collapsible>
+                    <AccordionItem value='item-1'className="border-b-0">
+                        <AccordionTrigger>
+                            <p>Order Date: {date.getMonth()}/{date.getDay()}/{date.getFullYear()}</p>
+                            <p>Order Total: ${getOrderTotal()}</p>
+                        </AccordionTrigger>
+                        <AccordionContent>
+                            <div key="header"  className="flex justify-between text-xl font-bold border-b-2 border-secondary mb-2 pb-2">
+                                <p className='w-1/3 text-start'>Product</p>
+                                <p className='w-1/3 text-center'>Quantity</p>
+                                <p className='w-1/3 text-end'>Price</p>
+                            </div>
+                            {order.products.map(product => (
+                                <div key={product.productId} className="flex justify-between">
+                                    <p className='w-1/3 text-start'>{product.productName}</p>
+                                    <p className='w-1/3 text-center'>{product.quantity}</p>
+                                    <p className='w-1/3 text-end'>${product.price}</p>
+                                </div>
+                            ))}
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
             </CardContent>
         </Card>
     );
